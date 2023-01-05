7df415b7bf80a36317fd91cd6bd7a2f15b36c4aa
Bryan A Quero <bryanq2008@live.com>
2023-01-05 15:54:58 -0500

    Commit #5

--- index.js
+++ index.js
@@ -1,378 +1,316 @@
-const mysql = require('mysql');
-const inquirer = require('inquirer');
-const cTable = require('console.table');
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const cTable = require("console.table");
 
-// Create connection to database called employees
+
+
 const connection = mysql.createConnection({
-    host: 'localhost',
-    // local port
-    port: 3306,
-    // Your username
-    user: "root",
-    // Your password
-    password: "@@22@MySql@23@",
 
-    // Name of database
-    database: "employees_db"
-
+  host: "localhost",
+  // port
+  port: 3306,
+  // username
+  user: "root",
+  // password
+  password: "@@22@MySql@@22@",
+  database: "employees_db"
 });
 
 
-// The connection.connect() method takes a callback function as an argument, which is executed when the connection to the database is established
 connection.connect((err) => {
-    if (err) throw err;
-    runStart();
+  if (err) throw err;
+
+  runSearch();
 });
 
-// This is the runStart function that starts the application.
-
-function runStart() {
-
-  inquirer
-    .prompt({
-      name: "action",
-      type: "list",
-      message: "What would you like to do?",
-      choices: [
-        "View All Employees",
-        "Add Employee",
-        "Remove Employee",
-        "Update Employee Role",
-        "Update Employee Manager",
-        "View All Roles",
-        "Add Role",
-        "Remove Role",
-        "View All Departments",
-        "Add Department",
-        "Remove Department",
-        "View Total Budget",
-        "Exit"
+// code
+function runSearch() {
+inquirer
+  .prompt({
+    name: "selection",
+    type: "list",
+    message: "What would you like to do?",
+    choices: 
+      [
+          "View All Employees",
+          "View Department",
+          "View role", 
+          "Add Employee",
+          "Add Department",
+          "Add Role", 
+          "Update Role",
       ]
-    })
-    .then(function(answer) {
-      switch (answer.action) {
-      case "View All Employees":
-        viewAllEmployees();
-        break;
-
-      case "Add Employee":
-        addEmployee();
-        break;
-
-      case "Remove Employee":
-        removeEmployee();
-        break;
-
-      case "Update Employee Role":
-        updateEmployeeRole();
-        break;
-
-      case "Update Employee Manager":
-        updateManager();
-        break;
-
-      case "View All Roles":
-        viewAllRoles();
-        break;
-
-      case "Add Role":
-        addRole();
-        break;
-
-      case "Remove Role":
-        removeRole();
-        break;
-
-      case "View All Departments":
-        viewAllDepartments();
-        break;
-
-      case "Add Department":
-        addDepartment();
-        break;
-
-      case "Remove Department":
-        removeDepartment();
-        break;
-
-      case "View Total Budget":
-        viewTotalBudget();
-        break;
-
-      case "Exit":
-        connection.end();
-        break;
-      }
-    });
-}
-
-// This is addNewEmployee function prompts the user to enter the first and last name, role ID, and manager ID (optional) for the new employee and then inserts this information into the employees table using a MySQL INSERT query.
-function viewAllEmployee(){
-    connection.query(
-      "SELECT employee.id, employee.first_name, employee.last_name, employee.role_id, employee.manager_id, role.title, role.salary, role.id, department.id FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id",
-      
-      function(err, res, fields) {
-
-        if (err) throw err;
-        console.table(res);
-        runStart();
-      }
-
-    );
-};
-
-// This is viewAllEmployeesByDepartment function prompts the user to enter the department name and then displays all employees in that department using a MySQL SELECT query.
-function viewAllEmployeesByDepartment(){
-    connection.query(
-      "SELECT employee.id, employee.first_name, employee.last_name, employee.role_id, employee.manager_id, role.title, role.salary, role.id, department.id FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id",
-      
-      function(err, res, fields) {
-
-        if (err) throw err;
-        console.table(res);
-        runStart();
-      }
-
-    )
-
-
-}
-
-// This is viewAllEmployeesByManager function prompts the user to enter the manager's name and then displays all employees that report to that manager using a MySQL SELECT query.
-
-function viewAllEmployeesByManager(){
-
-    connection.query(
-      "SELECT employee.id, employee.first_name, employee.last_name, employee.role_id, employee.manager_id, role.title, role.salary, role.id, department.id FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id",
-      
-      function(err, res, fields) {
-
-        if (err) throw err;
-        console.table(res);
-        runStart();
-      }
-
-    );
-
-
-};
-
-// This is addEmployee function prompts the user to enter the first and last name, role ID, and manager ID (optional) for the new employee and then inserts this information into the employees table using a MySQL INSERT query.
-
-function addEmployee(){
-      
-      connection.query(
-        "SELECT employee.id, employee.first_name, employee.last_name, employee.role_id, employee.manager_id, role.title, role.salary, role.id, department.id FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id",
-        
-        function(err, res, fields) {
-  
-          if (err) throw err;
-          console.table(res);
-          runStart();
-        }
-  
-      );
-  
-
-};
-
-// This is removeEmployee function prompts the user to enter the first and last name of the employee to be removed and then deletes that employee from the employees table using a MySQL DELETE query.
-
-function removeEmployee(){
-
-    connection.query(
-      "SELECT employee.id, employee.first_name, employee.last_name, employee.role_id, employee.manager_id, role.title, role.salary, role.id, department.id FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id",
-      
-      function(err, res, fields) {
-
-        if (err) throw err;
-        console.table(res);
-        runStart();
-      }
-
-    );
-
-
-};
-
-
-// This is updateEmployeeRole function prompts the user to enter the first and last name of the employee whose role is to be updated and then updates the employee's role using a MySQL UPDATE query.
-
-function updateEmployeeRole(){
-
-    connection.query(
-      "SELECT employee.id, employee.first_name, employee.last_name, employee.role_id, employee.manager_id, role.title, role.salary, role.id, department.id FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id",
-      
-      function(err, res, fields) {
-
-        if (err) throw err;
-        console.table(res);
-        runStart();
-      }
-
-    );
-
-};
-
-// This is updateManager function prompts the user to enter the first and last name of the employee whose manager is to be updated and then updates the employee's manager using a MySQL UPDATE query.
-
-function updateManager(){
-
-    connection.query(
-      "SELECT employee.id, employee.first_name, employee.last_name, employee.role_id, employee.manager_id, role.title, role.salary, role.id, department.id FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id",
-      
-      function(err, res, fields) {
-
-        if (err) throw err;
-        console.table(res);
-        runStart();
-      }
-
-    );
-
-}
-
-// This is viewAllRoles function displays all roles in the company using a MySQL SELECT query.
-
-function viewAllRoles(){
-  
-      connection.query(
-        "SELECT employee.id, employee.first_name, employee.last_name, employee.role_id, employee.manager_id, role.title, role.salary, role.id, department.id FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id",
-        
-        function(err, res, fields) {
-  
-          if (err) throw err;
-          console.table(res);
-          runStart();
-        }
-  
-      );
-  
-};
-
-// This is addRole function prompts the user to enter the name, salary, and department ID for the new role and then inserts this information into the roles table using a MySQL INSERT query.
-
-function addRole(){
-  
-      connection.query(
-        "SELECT employee.id, employee.first_name, employee.last_name, employee.role_id, employee.manager_id, role.title, role.salary, role.id, department.id FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id",
-        
-        function(err, res, fields) {
-  
-          if (err) throw err;
-          console.table(res);
-          runStart();
-        }
-  
-      );
-  
-  };
-
-// This is removeRole function prompts the user to enter the name of the role to be removed and then deletes that role from the roles table using a MySQL DELETE query.
-
-function removeRole(){
-
-    connection.query(
-      "SELECT employee.id, employee.first_name, employee.last_name, employee.role_id, employee.manager_id, role.title, role.salary, role.id, department.id FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id",
-      
-      function(err, res, fields) {
-
-        if (err) throw err;
-        console.table(res);
-        runStart();
-      }
-
-    );
-
-};
-
-// This is viewAllDepartments function displays all departments in the company using a MySQL SELECT query.
-
-function viewAllDepartments(){
-  
-      connection.query(
-        "SELECT employee.id, employee.first_name, employee.last_name, employee.role_id, employee.manager_id, role.title, role.salary, role.id, department.id FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id",
-        
-        function(err, res, fields) {
-  
-          if (err) throw err;
-          console.table(res);
-          runStart();
-        }
-  
-      );
-  
-  }
-
-// This is addDepartment function prompts the user to enter the name of the new department and then inserts this information into the departments table using a MySQL INSERT query.
-
-function addDepartment(){
+  })
+  .then(function(answer) {
+      console.log(answer);
     
-        connection.query(
-          "SELECT employee.id, employee.first_name, employee.last_name, employee.role_id, employee.manager_id, role.title, role.salary, role.id, department.id FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id",
-          
-          function(err, res, fields) {
-    
-            if (err) throw err;
-            console.table(res);
-            runStart();
-          }
-    
-        );
-    
+    if (answer.selection === "View All Employees") {
+      viewAll();
     }
+    else if(answer.selection === "View Department") {
+      viewDepts();
 
-// This is removeDepartment function prompts the user to enter the name of the department to be removed and then deletes that department from the departments table using a MySQL DELETE query.
+    } 
+    else if(answer.selection === "View role") {
+      viewrole();
 
-function removeDepartment(){
-  
-      connection.query(
-        "SELECT employee.id, employee.first_name, employee.last_name, employee.role_id, employee.manager_id, role.title, role.salary, role.id, department.id FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id",
-        
-        function(err, res, fields) {
-  
-          if (err) throw err;
-          console.table(res);
-          runStart();
-        }
-  
-      );
-  
-  }
+    }
+    else if(answer.selection === "Add Employee") {
+      addEmployee();
 
-// This is viewTotalBudget function displays the total utilized budget of a department -- the combined salaries of all employees in that department -- using a MySQL SELECT query.
+    }
+    else if(answer.selection === "Add Department") {
+      addDept();
 
-function viewTotalBudget(){
+    }
+    else if(answer.selection === "Add Role") {
+      addRole();
 
-    connection.query(
-      "SELECT employee.id, employee.first_name, employee.last_name, employee.role_id, employee.manager_id, role.title, role.salary, role.id, department.id FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id",
-      
-      function(err, res, fields) {
+    }
+    else if(answer.selection === "Update Role") {
+      updateRole();
 
-        if (err) throw err;
-        console.table(res);
-        runStart();
-      }
-
-    );
-
+    }else{
+      connection.end();
+    }
+  });
 }
 
-// This is exit function exits the application.
-
-function exit(){
-  
-      connection.query(
-        "SELECT employee.id, employee.first_name, employee.last_name, employee.role_id, employee.manager_id, role.title, role.salary, role.id, department.id FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id",
-        
-        function(err, res, fields) {
-  
-          if (err) throw err;
-          console.table(res);
-          runStart();
-        }
-  
-      );
-  
+//View All Employees Function
+function viewAll() {
+    connection.query(
+      "SELECT employee.id, employee.first_name, employee.last_name, employee.role_id, employee.manager_id, role.title, role.salary, role.id, department.id FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id", 
+      function(err, result, fields) {
+        if (err) throw err;
+        console.table(result);
+        // re-prompt the user for another selection
+        runSearch();
+      }
+    );
   };
 
+function viewrole() {
+connection.query(
+"SELECT role.id, role.title, role.salary, role.department_id, department.id, department.name FROM role LEFT JOIN department on role.department_id = department.id",
+function(err, result, fields) {
+   if (err) throw err;
+   console.table(result);
+   // re-prompt the user for another selection
+   runSearch();
+ }
+); };
+
+function viewDepts() {
+connection.query("SELECT * FROM department", function(err, result, fields) {
+    if (err) throw err;
+    console.table(result);
+    // re-prompt the user for another selection
+    runSearch();
+  }
+); };
+
+
+const roleChoices = [];
+const empChoices = [];
+const deptChoices = [];
+
+function lookuprole(){  
+  
+  connection.query("SELECT * FROM role", function (err, data) {
+      if (err) throw err;
+      for (i = 0; i < data.length; i++) {
+          roleChoices.push(data[i].id + "-" + data[i].title)
+      }
+   })
+  }
+
+function lookupEmployee(){  
+   connection.query("SELECT * FROM employee", function (err, data) {
+       if (err) throw err;
+       for (i = 0; i < data.length; i++) {
+           empChoices.push(data[i].id + "-" + data[i].first_name+" "+ data[i].last_name)
+       }
+   }) 
+  }
+
+function lookupDepts(){
+connection.query("SELECT * FROM department", function (err, data) {
+  if (err) throw err;
+  for (i = 0; i < data.length; i++) {
+      deptChoices.push(data[i].id + "-" + data[i].name)
+  }
+})
+}
+
+function addEmployee() {
+
+  lookUprole()
+  lookupEmployee()
+
+  inquirer.prompt([
+  {
+    name: "first name",
+    type: "input",
+    message: "What is the employee's first name?"
+  },
+
+  {
+      name: "Last name",
+      type: "input",
+      message: "What is the employee's last name?"
+  },
+
+  {
+      name: "role",
+      type: "list",
+      message: "What is the employee's role?",
+      choices: roleChoices 
+    },
+
+    {
+      name: "reportingTo",
+      type: "list",
+      message: "Who is the employee's manager?",
+      choices: empChoices
+    }
+  
+   ]).then(function(answer) {
+    const getRoleId =answer.role.split("-")
+    const getReportingToId=answer.reportingTo.split("-")
+    const query = 
+    `INSERT INTO employee (first_name, last_name, role_id, manager_id)
+     VALUES ('${answer.first_name}','${answer.last_name}','${getRoleId[0]}','${getReportingToId[0]}')`;
+    connection.query(query, function(err, res) {
+      console.log(`new employee ${answer.first_name} ${answer.last_name} added!`)
+    });
+    runSearch();
+  });
+};
+
+function addRole() {
+
+lookuprole()
+lookupEmployee()
+lookupDepts()
+
+inquirer.prompt([
+{
+  name: "role",
+  type: "input",
+  message: "Enter the role you would like to add:"
+},
+
+{
+    name: "dept",
+    type: "list",
+    message: "In what department would you like to add this role?",
+    choices: deptChoices
+},
+
+{
+  name: "salary",
+  type: "number",
+  message: "Enter the role's salary:"
+},
+
+ ]).then(function(answer) {
+   console.log(`${answer.role}`)
+  const getDeptId =answer.dept.split("-")
+  const query = 
+  `INSERT INTO role (title, salary, department_id)
+   VALUES ('${answer.role}','${answer.salary}','${getDeptId[0]}')`;
+  connection.query(query, function(err, res) {
+    console.log(`<br>-----new role ${answer.role} added!------`)
+  });
+  runSearch();
+});
+};
+
+function addDept() {
+
+lookuprole()
+lookupEmployee()
+lookupDepts()
+
+inquirer.prompt([
+{
+  name: "dept",
+  type: "input",
+  message: "Enter the department you would like to add:"
+}
+]).then(function(answer) {
+  const query = 
+  `INSERT INTO department (name)
+   VALUES ('${answer.dept}')`;
+  connection.query(query, function(err, res) {
+    console.log(`-------new department added: ${answer.dept}-------`)
+  });
+  runSearch();
+});
+};
+
+
+function updateRole() {
+connection.query('SELECT * FROM employee', function (err, result) {
+  if (err) throw (err);
+  inquirer
+    .prompt([
+      {
+        name: "employeeName",
+        type: "list",
+
+        message: "Which employee's role is changing?",
+        choices: function () {
+          const employeeArray = [];
+          result.forEach(result => {
+            employeeArray.push(
+              result.last_name
+            );
+          })
+          return employeeArray;
+        }
+      }
+    ])
+ 
+    .then(function (answer) {
+      console.log(answer);
+      const name = answer.employeeName;
+    
+      connection.query("SELECT * FROM role", function (err, res) {
+        inquirer
+          .prompt([
+            {
+              name: "role",
+              type: "list",
+              message: "What is their new role?",
+              choices: function () {
+                const roleArray = [];
+                res.forEach(res => {
+                  roleArray.push(
+                    res.title)
+                })
+                return roleArray;
+              }
+            }
+          ]).then(function (roleAnswer) {
+            const role = roleAnswer.role;
+            console.log(role);
+            connection.query('SELECT * FROM role WHERE title = ?', [role], function (err, res) {
+              if (err) throw (err);
+              let roleId = res[0].id;
+ 
+              let query = "UPDATE employee SET role_id = ? WHERE last_name =  ?";
+              let values = [parseInt(roleId), name]
+      
+              connection.query(query, values,
+                function (err, res, fields) {
+                  console.log(`You have updated ${name}'s role to ${role}.`)
+                })
+              viewAll();
+            })
+          })
+      })
+      //})
+    })
+})
+};
